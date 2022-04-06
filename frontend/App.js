38c17e7d4519594dd2d2a4583fdc00fda710aa44
Nitesh Rathore <41752721+niteshrathore123@users.noreply.github.com>
2022-04-06 16:20:53 +0530

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todolist from './Component/Todolist';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
